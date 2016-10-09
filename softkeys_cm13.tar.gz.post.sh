5cb83fe252803324c56b7d97c8d08c85aecc0c24
ralphsoftoss <rvreman8@gmail.com>
2016-10-09 04:59:55 -0400

    Add files via upload

--- softkeys_cm13.tar.gz.post.sh
+++ softkeys_cm13.tar.gz.post.sh
@@ -1,3 +1,11 @@
-echo SOFTKEYS CM13
+#!/system/xbin/bash
 mount -o remount,rw /system
+[ ! -e /etc/uninstaller.d ] && mkdir /etc/uninstaller.d
+echo SOFTKEYS CM13
+echo Backing up build.prop...
+cp /system/build.prop /system/backbuild.prop
 echo qemu.hw.mainkeys=0 >> /system/build.prop
+echo Notice! Recover backbuild.prop from /system to build.prop if you are experiencing issues! This message will be dismissed in 7 seconds.
+sleep 7
+echo Finished, rebooting...
+reboot
